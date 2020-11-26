09d37fe9f43e932f124a52aae39fd56baedbac50
Alexandre Nabas Pantaleão <alexandre.nabas@hotmail.com>
2020-11-26 13:23:24 -0300

    Inicio do Projeto do Jogo de Xadrez

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Tabuleiro;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao pos = new Posicao(3, 2);
+            Console.WriteLine("Posicao: "+pos);
+        }
+    }
+}

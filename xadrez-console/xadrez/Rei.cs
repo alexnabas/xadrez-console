34e975bea901c7c6a57feabc8e8420acc517e925
Alexandre Nabas Pantaleão <alexandre.nabas@hotmail.com>
2020-12-02 13:09:19 -0300

    Comecando a colocar pecas no tabuleiro

---
+++ xadrez-console/xadrez/Rei.cs
@@ -0,0 +1,16 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class Rei : Peca
+    {
+        public Rei(Tabuleiro t, Cor c) : base(t, c)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "R";
+        }
+    }
+}
---
+++ xadrez-console/xadrez/Torre.cs
@@ -0,0 +1,16 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class Torre : Peca
+    {
+        public Torre(Tabuleiro t, Cor c) : base(t, c)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "T";
+        }
+    }
+}
